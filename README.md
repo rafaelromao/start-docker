c80b1bb9b82cbc19052a0fc0a53e27830fadbad5
Rafael Romão <rafaelromao@gmail.com>
2015-07-10 12:34:43 -0300

    Updated to execute only the necessary steps

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # start-docker
-Powershell module to run "boot2docker start" and "boot2docker shellinit | invoke-expression" silently
+Powershell command to prepare docker commands to use via powershell
 
-- Install the module using Install-Module.bat.
-- On the powershell window, just run "start-docker".
+## Prerequisites
+- boot2docker must be installed (no need to run the "boot2docker Start" script)
+- Git bin folder must be on the system PATH
+- Powershell execution policy must be set to remotesigned, at least
+- Hyper-V must be disabled
+
+## How to install it
+- Download and unzip this repository
+- Run Install-Module.bat to install the start-docker module on powershell
+
+## What it will do?
+1) Download the ISO and create the boot2docker VM, if necessary
+2) Start the boot2docker VM, if necessary
+3) Initialize the shell to run docker commands
+
+## How to use it
+- Run "start-docker"
+- Run "docker ps" to see if all is okay
--- Start-Docker.psm1
+++ Start-Docker.psm1
@@ -1,7 +1,27 @@
-function Start-Docker { 
-	boot2docker start 2> $null | Out-Null
-	$shellinits = boot2docker shellinit 2> $null
-	ForEach ($shellinit in $shellinits) {
-		Invoke-Expression $shellinit 2> $null | Out-Null
+function Start-Docker() { 
+	# Check if boot2docker VM already exists
+	$status = boot2docker.exe status 2>&1 | Out-String
+	if ($status.Contains("error")) {
+		# Create boot2docker VM
+		Write-Host "Creating boot2docker VM..."
+		boot2docker.exe init 2> $null | Out-Null
+	}
+	# Check if boot2docker VM is already running
+	$status = boot2docker.exe status 2>&1 | Out-String
+	if ($status.Contains("poweroff")) {
+		# Start boot2docker VM
+		Write-Host "Starting boot2docker VM..."
+		boot2docker.exe start 2> $null | Out-Null
+	}
+	# Check if boot2docker VM is already running
+	$status = boot2docker.exe status 2>&1 | Out-String
+	if ($status.Contains("running")) {
+		# Initialize the shell silently
+		$shellinits = boot2docker shellinit 2> $null
+		ForEach ($shellinit in $shellinits) {
+			Invoke-Expression $shellinit 2> $null | Out-Null
+		}
+	} else {
+		Write-Host "Could not start the boot2docker-vm"
 	}
 }
